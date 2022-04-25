233bde8f8824bd79bb9233b8fcd3a1bba27bc981
Jc <Jc@email.com>
2022-04-25 17:14:54 +0100

    2 typed inputs and submit button. post req to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { greet } from "./utils/greet";
+import PasteSubmission from "./components/PasteSubmission";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return(
+    <PasteSubmission />
+  );
 }
 
 export default App;
