2a83c78be364c54320b3ff88df00a6e300536b7d
JacobCutts13 <30525465+JacobCutts13@users.noreply.github.com>
2022-05-11 14:24:21 +0100

    Revert "C4A6 whole-project PR for review"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import PasteSubmission from "./components/PasteSubmission";
-import View from "./components/View";
-import Header from "./components/Header";
-import "./style.css";
+import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/submit" element={<PasteSubmission />} />
-          <Route path="/" element={<View />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <h1>{greet("World")}</h1>;
 }
 
 export default App;
